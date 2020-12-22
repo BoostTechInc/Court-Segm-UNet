913b9d787d0b0e6269e4887887b83fddbc081772
darkAlert <darkangel-nwo@yandex.ru>
2020-12-22 15:08:37 +0300

    fix n_classes

--- utils/postprocess.py
+++ utils/postprocess.py
@@ -28,12 +28,12 @@ def onehot_to_image(masks, n_classes=4):
 
     # Generate mapping:
     mapping = {}
-    if n_classes == 4:
+    if n_classes == 3:
         mapping[1] = (0, 255, 0)
         mapping[2] = (255, 0, 0)
         mapping[3] = (0, 0, 255)
         mapping[4] = (255, 255, 255)
-    elif n_classes == 9:
+    elif n_classes == 7:
         mapping[1] = (0, 255, 0)
         mapping[2] = (255, 0, 0)
         mapping[3] = (0, 0, 255)
